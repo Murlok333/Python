33250cf9547060d817277363b1bf2bf0d8167c1b
Андрей Боев <a.boev.it@gmail.com>
2024-10-22 17:29:43 +0300

    Разбор введения

---
+++ 1. Введение/main.py
@@ -0,0 +1,20 @@
+'''
+print('Text 1', 'Text 2', 'Text 3', 'Text 4', end=' ')
+print('Text 5', 'Text 6', 'Text 7', 'Text 8')
+'''
+
+
+'''
+print('\'Text 1\'', '\'Text 2\'', '\'Text 3\'', 'Text 4', sep='\n')
+'''
+
+'''
+name = input('Введите ваше имя: ')
+age = input('Введите ваш возраст: ')
+print(f'Меня зовут {name}, мне {age} лет.')
+'''
+from math import sqrt
+
+num1 = int(input('Введите первое число: '))
+num2 = int(input('Введите второе число: '))
+print(sqrt(num1 + num2))
