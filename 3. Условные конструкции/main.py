8a1216643312cf51cb24ff0e5baf6ad41586ae95
Андрей Боев <a.boev.it@gmail.com>
2024-11-19 17:32:46 +0300

    3. Условные конструкции

---
+++ 3. Условные конструкции/main.py
@@ -0,0 +1,15 @@
+age = 18
+if age >= 18:
+    print("Вы совершеннолетний")
+else:
+    print("Вы несовершеннолетний")
+
+score = 75
+if score >= 90:
+    print("Отлично")
+elif score >= 70:
+    print("Хорошо")
+elif score >= 50:
+    print("Удовлетворительно")
+else:
+    print("Неудовлетворительно")
